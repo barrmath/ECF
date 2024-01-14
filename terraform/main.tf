fe0c604182a247c05528e1084f60e7dad391a662
tux <mathieu.barrios@outlook.com>
2024-01-14 06:53:13 +0100

    modif fichier main.tf 2 instances EC2 sont ouvertes. WIP : voir avec VAULT ppour gerer les clé SSH et autres secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,20 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "mongo_DB" {
   ami           = "ami-00a4e06732205ac29"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "MongoDB"
+  }
+}
+
+resource "aws_instance" "spark_cluster" {
+  ami           = "ami-00a4e06732205ac29"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Apache_Spark"
   }
 }
