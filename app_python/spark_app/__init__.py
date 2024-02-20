d603583e0a4de9138519119de743b69c2a831ca3
tux <mathieu.barrios@outlook.com>
2024-02-20 18:21:19 +0100

    modification __init__.py

--- app_python/spark_app/__init__.py
+++ app_python/spark_app/__init__.py
@@ -3,12 +3,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, static_folder="static")
-
     app.config.from_object("config")
-
     # routage des pages web
-
     with app.app_context():
         import spark_app.views
-
     return app
